97928dac2d70fe3459fafac64a2c58819b4ff196
LMERLAUD <louis.merlaud@ynov.com>
2022-11-07 16:19:01 +0100

    TP - WIK-DPS-TP03

--- devops_tp1/src/main.rs
+++ devops_tp1/src/main.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::ptr::null;
 use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
@@ -41,14 +40,15 @@ fn handle_connection(mut stream: TcpStream) {
     res.push_str("}");
     println!("{}",connection);
 
-    if connection == "GET /PING HTTP/1.1" {
+    if connection == "GET /PING HTTP/1.0" {
         let status_line = "HTTP/1.1 200 OK";
-        let contents = res; //fs::read_to_string("hello.html").unwrap();
+        let contents = res;
         let length = contents.len();
 
         let response = format!(
             "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
         );
+        println!("Hostname: {:?}", hostname::get());
 
         stream.write_all(response.as_bytes()).unwrap();
     } else {
@@ -60,14 +60,14 @@ fn handle_connection(mut stream: TcpStream) {
                 "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
             );
             stream.write_all(response.as_bytes()).unwrap();
-        } else {
-            let status_line = "HTTP/1.1 500 UNEXPECTED ERROR";
-            let contents = fs::read_to_string("500.html").unwrap();
-            let length = contents.len();
-            let response = format!(
-                "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-            );
-            stream.write_all(response.as_bytes()).unwrap();
-        }
+        } //else {
+        //     let status_line = "HTTP/1.1 500 UNEXPECTED ERROR";
+        //     let contents = fs::read_to_string("500.html").unwrap();
+        //     let length = contents.len();
+        //     let response = format!(
+        //         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        //     );
+        //     stream.write_all(response.as_bytes()).unwrap();
+        // }
     }
 }
